138459e6619d30d5ee2892b56bcf9c6f24608287
Clive Munro <clive@clive.local>
2018-09-19 18:22:29 +0200

    Changed the URL for DV Code

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -5,26 +5,145 @@
 const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
+const SpotifyWebApi = require('spotify-web-api-node');
  
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
- 
+
+const userId = '5butodgv0iy83aosjzhw23f76';
+const spotify = new SpotifyWebApi();
+spotify.setAccessToken('BQDbw-RCGX5RxhJAWAxXk9JMTowrB3m1vo_uEOczIIXzoMRjE07NEyliJIHmM3wXO-wqKKGSMiaT_VMW1OTksokKpDDkd5Hdvia9UxUkPKpowbp1nI4Cw1UeLUQAPFWp3JO7oIxJ4OosxXd4oA4c6V6OtNmEGwTWy33q3aPGSJTJr7So1pkrszzdfH5bCQQbPg');
+
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
   const agent = new WebhookClient({ request, response });
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
- 
+  
   function welcome(agent) {
     agent.add(`Welcome to my agent!`);
   }
  
-  function playlistHandler(agent) {
-    agent.add(`We are now in the playlist handler :)`);
+  function hasValidParameters(parameters) {
+    if (!parameters['track_attribute']) {
+      console.error("Dialogflow Request does not contain the 'track_attribute' parameter: " + parameters);
+      return false;
+    }
+    if (!parameters['attribute_value']) {
+      console.error("Dialogflow Request does not contain the 'attribute_value' parameter: " + parameters);
+      return false;
+    }
+
+    if (!parameters['genre']) {
+      console.error("Dialogflow Request does not contain the 'genre' parameter: " + parameters);
+      return false;
+    }
+
+    return true;
+  }
+
+  function newUUID(){
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+  }
+
+  function getRecommendations(trackAttribute, attributeValue, genre){
+    const recommendationParams = {limit: 100, seed_genres: [genre]};
+    recommendationParams['target_' + trackAttribute] = attributeValue/100;
+    return spotify.getRecommendations(recommendationParams);
+  }
+
+  function normalizeSpotifyResponse(response){
+    return response.body;
+  }
+
+  function getTrackUrisFromRecommendations (tracks) {
+    let trackUris = [];
+
+    for (var i = 0; i < tracks.length; i++) {
+      const track = tracks[i];
+      trackUris.push(track.uri);
+    }
+
+    return trackUris;
+  }
+
+  function addTracksToPlaylist(playlist, trackUris){
+    return spotify.addTracksToPlaylist(userId, playlist.id, trackUris);
+  }
+
+  function createPlaylist(){
+    const playlistName = `Dynamic Playlist ${newUUID()}`;
+    return spotify.createPlaylist(userId, playlistName, { 'public' : true });
+  }
+
+  async function generatePlaylist(trackAttribute, attributeValue, genre){
+
+    try{
+      console.log("Getting Recommendations");
+      const recommendationResponse = await getRecommendations(trackAttribute, attributeValue, genre);
+      const recommendations = normalizeSpotifyResponse(recommendationResponse);
+      const trackUris = getTrackUrisFromRecommendations(recommendations.tracks)
+      console.log(`Found ${trackUris.length} recommendations.`);
+
+      console.log('Creating Playlist');
+      const playlistResponse = await createPlaylist();
+      const playlist = normalizeSpotifyResponse(playlistResponse);
+      console.log('Playlist Created: ' + playlist);
+
+      console.log('Adding Tracks to Playlist');
+      await addTracksToPlaylist(playlist, trackUris);
+      console.log('Tracks Added');
+
+      return playlist.external_urls.spotify;
+    } catch(err) {
+        console.log('Something went wrong!', err);
+        throw err;
+    }
+  }
+
+  function buildPlaylistResponse(parameters, playlistUrl){
+    return `Here is your playlist with ${parameters['track_attribute']} @ ${parameters['attribute_value']}%: ${playlistUrl}`;
+  }
+
+  async function playlistHandler(agent) {
+    const parameters = agent.parameters;
+    console.log(parameters);
+
+    if (!hasValidParameters(parameters)) {
+      return fallback(agent);
+    }
+
+    let playlistUrl = await generatePlaylist(parameters['track_attribute'], parameters['attribute_value'], parameters['genre']);
+
+    agent.add(buildPlaylistResponse(parameters, playlistUrl));
   }
  
+  function buildOAuthResponse(parameters){
+    const trackAttribute = encodeURIComponent(parameters['track_attribute']);
+    const attributeValue = encodeURIComponent(parameters['attribute_value']);
+    const genre = encodeURIComponent(parameters['genre']);
+    const oAuthUrl = `https://spotify-aqua-beagle.netlify.com/?track_attribute=${trackAttribute}&attribute_value=${attributeValue}&genre=${genre}`;
+    return `Click here to view your playlist. ${oAuthUrl}`;
+  }
+
+  function oAuthHandler(agent){
+    const parameters = agent.parameters;
+    console.log(parameters);
+
+    if (!hasValidParameters(parameters)) {
+      return fallback(agent);
+    }
+
+    agent.add(buildOAuthResponse(parameters));
+  }
+
   function fallback(agent) {
     agent.add(`I didn't understand`);
     agent.add(`I'm sorry, can you try again?`);
-}
+  }
 
   // // Uncomment and edit to make your own intent handler
   // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
